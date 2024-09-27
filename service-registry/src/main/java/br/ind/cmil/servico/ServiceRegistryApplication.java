ab566c880dae02256d773446af0a2047d17974f8
Abraão C Cassinda <dtimuila@gmail.com>
2024-09-27 14:45:26 -0300

    Strict desable

---
+++ service-registry/src/main/java/br/ind/cmil/servico/ServiceRegistryApplication.java
@@ -0,0 +1,15 @@
+package br.ind.cmil.servico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ServiceRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRegistryApplication.class, args);
+    }
+
+}
---
+++ service-registry/src/test/java/br/ind/cmil/servico/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ind.cmil.servico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
