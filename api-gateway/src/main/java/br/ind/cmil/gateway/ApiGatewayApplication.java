f5700ddef6ccf1959158ec337624a1578b03905e
Abraão C Cassinda <dtimuila@gmail.com>
2024-10-08 17:10:51 -0300

    primeniro git do patrimonio

--- api-gateway/src/main/java/br/ind/cmil/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/br/ind/cmil/gateway/ApiGatewayApplication.java
@@ -2,12 +2,19 @@ package br.ind.cmil.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 
+    /**
+     * @Bean public HttpClient httpClient() { return
+     * HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
+    }
+     */
 }
--- api-gateway/src/main/java/br/ind/cmil/gateway/securitys/configs/WebMvcConfig.java
+++ api-gateway/src/main/java/br/ind/cmil/gateway/securitys/configs/WebMvcConfig.java
@@ -3,39 +3,71 @@ package br.ind.cmil.gateway.securitys.configs;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 /**
  *
  * @author Administrativo
  */
 @Configuration
-
 public class WebMvcConfig {
 
     @Value("${cors.origins}")
-    private String origens;
+    private String corsOrigins;
 
     @Bean
-    public WebMvcConfigurer corsConfig() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins(origens)
-                        .allowedMethods(HttpMethod.GET.name(),
-                                HttpMethod.POST.name(),
-                                HttpMethod.DELETE.name())
-                        .allowedHeaders(HttpHeaders.CONTENT_TYPE,
-                                HttpHeaders.AUTHORIZATION);
-            }
-        };
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.addAllowedOrigin(corsOrigins);
+        corsConfig.addAllowedMethod("*");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
     }
 
     /**
+     * @Bean public CorsFilter corsWebFilter() { CorsConfiguration corsConfig =
+     * new CorsConfiguration(); corsConfig.addAllowedOrigin(corsOrigins);
+     * corsConfig.addAllowedHeader("*");
+     * corsConfig.addAllowedMethod(HttpMethod.GET);
+     * corsConfig.addAllowedMethod(HttpMethod.POST);
+     * corsConfig.addAllowedMethod(HttpMethod.PUT);
+     * corsConfig.addAllowedMethod(HttpMethod.PATCH);
+     * corsConfig.addAllowedMethod(HttpMethod.OPTIONS);
+     * corsConfig.setAllowCredentials(false);
+     *
+     * UrlBasedCorsConfigurationSource source = new
+     * UrlBasedCorsConfigurationSource();
+     * source.registerCorsConfiguration("/**", corsConfig);
+     *
+     * return new CorsFilter(source); }
+     *
+     * @Value("${cors.origins}")
+     * private String origens;
+     *
+     * @Bean
+     * public WebMvcConfigurer corsConfig() {
+     * return new WebMvcConfigurer() {
+     * @Override
+     * public void addCorsMappings(CorsRegistry registry) {
+     * registry.addMapping("/**")
+     * .allowedOrigins(origens)
+     * .allowedMethods(HttpMethod.GET.name(),
+     * HttpMethod.POST.name(),
+     * HttpMethod.DELETE.name())
+     * .allowedHeaders(HttpHeaders.CONTENT_TYPE,
+     * HttpHeaders.AUTHORIZATION);
+     * }
+     * };
+     * }
+     *
+     *
      * @Override public void addCorsMappings(CorsRegistry registry) {
      *
      * registry.addMapping("/**") .allowedOrigins("http://localhost:4200/**")
