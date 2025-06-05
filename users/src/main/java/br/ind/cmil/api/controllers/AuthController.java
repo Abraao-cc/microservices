c39c3119b160a542fe4e9006a8f2907120fda247
abraao <abraao@acc>
2025-06-05 20:40:17 -0300

    Atualizaço da classe usuário

--- users/src/main/java/br/ind/cmil/api/controllers/AuthController.java
+++ users/src/main/java/br/ind/cmil/api/controllers/AuthController.java
@@ -1,18 +1,13 @@
 package br.ind.cmil.api.controllers;
 
-import br.ind.cmil.api.persistence.model.User;
 import br.ind.cmil.api.dto.LoginRequestDTO;
 import br.ind.cmil.api.dto.RegisterUserDTO;
 import br.ind.cmil.api.dto.ResponseDTO;
-import br.ind.cmil.api.security.TokenServiceImp;
-import br.ind.cmil.api.respositories.UserRepository;
 import br.ind.cmil.api.service.AuthorizationService;
 import br.ind.cmil.api.service.UserService;
 import jakarta.validation.Valid;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -31,8 +26,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
+   
+
     private final UserService userService;
     private final AuthorizationService authorizationService;
+
+    public AuthController(UserService userService, AuthorizationService authorizationService) {
+        this.userService = userService;
+        this.authorizationService = authorizationService;
+    }
+
+    
 
     @PostMapping("/login")
     public ResponseEntity<ResponseDTO> login(@Valid @RequestBody LoginRequestDTO body) {
--- users/src/main/java/br/ind/cmil/api/persistence/model/User.java
+++ users/src/main/java/br/ind/cmil/api/persistence/model/User.java
@@ -18,8 +18,7 @@ import org.springframework.format.annotation.DateTimeFormat;
  *
  * @author Administrativo
  */
-@Getter
-@Setter
+
 @Entity
 @Table(name = "tbl_users", uniqueConstraints = {
     @UniqueConstraint(columnNames = "firstName"),
@@ -60,5 +59,78 @@ public class User extends EntityID {
     public User(String id) {
         super.setId(id);
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDateTime registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public boolean isTokenExpired() {
+        return tokenExpired;
+    }
+
+    public void setTokenExpired(boolean tokenExpired) {
+        this.tokenExpired = tokenExpired;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+    
 
 }
